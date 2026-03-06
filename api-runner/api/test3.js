f5514452f9a21c1f152ab7246215cf2390c5af9a
Matthew Thiffault <matthewthiffault@macbookpro.lan>
2026-03-05 22:40:02 -0500

    Add test3 no imports

---
+++ api-runner/api/test3.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+    const pw = (req.body || {}).password;
+    const env = process.env.ADMIN_PASSWORD;
+    res.json({ pw, envSet: !!env, match: pw === env });
+}
